9e7f7e5374cd3348474c59b631efe1ad4cd0fc37
Mikaele <mikaelethainara@gmail.com>
2022-11-21 22:10:56 -0300

    cria template e home

--- resources/views/events/home.blade.php
+++ resources/views/events/home.blade.php
@@ -3,5 +3,19 @@
 @section('title', 'Home')
 
 @section('content')
-<h1>Home page</h1>
+
+  <div class="container">
+    <div class="row align-items-center">
+
+      <div class="col">
+      <h2>Facilidade no mundo das finanças</h2>
+      <br>
+      <p>Na FinHub você encontra uma bela curadoria de conteúdos relacionado a educação financeira que podem te ajudar a entender mais sobre o universo financeiro</p>
+      </div>
+
+      <div class="col">
+        <img src="images/home.png" id="home">
+      </div>
+    </div>
+  </div>
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,25 +10,28 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
         <!-- css da aplcação -->
         <link href="/css/styles.css" rel="stylesheet" type="text/css" />
-        <script src="/resources/js/script.js"></script>
+        <script src="/js/script.js"></script>
     </head>
     <body>
 
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="#">FinHub</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
             <li class="nav-item active">
-              <a class="nav-link" href="#">Home</a>
+              <a class="nav-link" href="#">Cursos</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Features</a>
+              <a class="nav-link" href="#">Influenciadores</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Pricing</a>
+              <a class="nav-link" href="#">Livros</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Podcast</a>
             </li>
           </ul>
         </div>
