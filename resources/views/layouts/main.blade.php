6676748b41a1b1ea7f8b0e75ed52d5b0aa49f70c
Tulio Ribeiro <tulio.ribeiromaia@gmail.com>
2022-11-26 10:24:20 -0300

    Adicionado rota para /livros

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -15,7 +15,9 @@
     <body>
 
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">FinHub</a>
+        <a class="navbar-brand" href="#">
+          <img src="/images/LogoFinHub.png" alt="FinHub">
+        </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -28,7 +30,7 @@
               <a class="nav-link" href="#">Influenciadores</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Livros</a>
+              <a class="nav-link" href="/livros">Livros</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Podcast</a>
