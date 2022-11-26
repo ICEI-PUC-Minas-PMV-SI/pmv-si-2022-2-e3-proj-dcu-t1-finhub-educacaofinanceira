e0e4fa4c44800d3885d223a1738c7531a236b3fc
Tulio Ribeiro <tulio.ribeiromaia@gmail.com>
2022-11-26 10:11:27 -0300

    Adicionado imagens e cadastrarLivros.blade.php

---
+++ resources/views/events/cadastrarLivros.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.main')
+
+@section('title', 'Livros')
+
+@section('content')
+
+<h1>Cadastrar Livros </h1>
+
+<h2>Titulo:*</h2>
+<h2>Autor(a):*</h2>
+<h2>Edição:*</h2>
+<h2>Ano:*</h2>
+<h2>N° de Páginas:*</h2>
+<h2>Idioma:*</h2>
+<h2>Editora:*</h2>
+<h2>Data Publicação:*</h2>
+<h2>Descrição:*</h2>
+
+
+
+@endsection
---
+++ resources/views/events/livros.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.main')
+
+@section('title', 'Livros')
+
+@section('content')
+
+<h1>Livros </h1>
+
+<button type="button" class="btn btn-light">Adicionar Livros</button>
+
+<div class="row row-cols-1 row-cols-md-3 g-3">
+  <div class="col">
+    <div class="card">
+      <img src="\images\livro_pai_rico_pai_pobre.jpg" class="card-img-top" alt="Pai Rico Pai Pobre">
+      <div class="card-body">
+        <h5 class="card-title">Pai Rico Pai Pobre</h5>
+        <p class="card-text">A escola prepara as crianças para o mundo real? Essa é a primeira pergunta</p>
+        <button type="button" class="btn btn-light">Saiba Mais</button>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="\images\livro_mil_ao_milhao.jpg" class="card-img-top" alt="Do Mil ao Milhão">
+      <div class="card-body">
+        <h5 class="card-title">Do Mil ao Milhão</h5>
+        <p class="card-text">Em seu primeiro livro, Thiago Nigro, criador da plataforma O Primo Rico...</p>
+        <button type="button" class="btn btn-light">Saiba Mais</button>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="\images\livro_me_poupe.jpg" class="card-img-top" alt="Me Poupe">
+      <div class="card-body">
+        <h5 class="card-title">Me Poupe</h5>
+        <p class="card-text">Como economizar no dia a dia? Como poupar mesmo ganhando pouco?</p>
+        <button type="button" class="btn btn-light">Saiba Mais</button>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
